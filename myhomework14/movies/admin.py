7cb9686d93bd99f9005ba4e160e7997117a5b6d5
Chrissy Lee <22chandl@gmail.com>
2021-12-02 17:52:44 +0900

    Movies 모델 생성

--- myhomework14/movies/admin.py
+++ myhomework14/movies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from movies.models import Movies
 
-# Register your models here.
+class MoviesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Movies, MoviesAdmin)
--- myhomework14/movies/models.py
+++ myhomework14/movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Movies(models.Model):
+    title = models.CharField(max_length=50)
+    director = models.CharField(max_length=20)
+    actors = models.CharField(max_length=30)
+    comments = models.TextField()
+    poster_file = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
