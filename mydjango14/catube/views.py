6588f7580120ff2871c672b6b274144424949b93
Chrissy Lee <22chandl@gmail.com>
2021-12-02 15:46:03 +0900

    CBV 예시 추가

--- mydjango14/catube/views.py
+++ mydjango14/catube/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from catube.models import Video
 
 
+# Class Based View (CBV) : 클래스 활용해서 view 함수 만들기
+# index = ListView.as_view(model=Video, template_name="catube/index.html")
+
+
+# Function Based View (FBV) : 함수 기반 view
 def index(request: HttpRequest) -> HttpResponse:
     qs = Video.objects.all()
     return render(
