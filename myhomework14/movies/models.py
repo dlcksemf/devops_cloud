fb60205f5108c95f680948a9436de0b86f0fe978
Chrissy Lee <22chandl@gmail.com>
2021-12-02 18:36:07 +0900

    django-imagekit 라이브러리를 활용한 썸네일 처리 / Movies에 /movies/ url 연결

--- myhomework14/movies/models.py
+++ myhomework14/movies/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from imagekit.models import ImageSpecField
+from pilkit.processors import ResizeToFill
 
 
 class Movies(models.Model):
@@ -7,4 +9,10 @@ class Movies(models.Model):
     actors = models.CharField(max_length=30)
     comments = models.TextField()
     poster_file = models.ImageField()
+    poster_file_thumb = ImageSpecField(
+        source="poster_file",
+        processors=[ResizeToFill(400, 800)],
+        format="JPEG",
+        options={"quality": 60},
+    )
     created_at = models.DateTimeField(auto_now_add=True)
