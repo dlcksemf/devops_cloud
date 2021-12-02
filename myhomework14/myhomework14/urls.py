873e06dd6fcaff03b62b9679d485eb7d4d9eda8e
Chrissy Lee <22chandl@gmail.com>
2021-12-02 17:28:36 +0900

    movies 앱 생성 및 등록

--- myhomework14/myhomework14/urls.py
+++ myhomework14/myhomework14/urls.py
@@ -18,8 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from movies.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('movies/', index),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
