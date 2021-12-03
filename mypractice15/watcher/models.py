e872701781cb1bde8db73952bb3cfcdb2ce0404a
Chrissy Lee <22chandl@gmail.com>
2021-12-03 17:53:33 +0900

    watcher 안에 movie 모델 생성

--- mypractice15/watcher/models.py
+++ mypractice15/watcher/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from imagekit.models import ImageSpecField
+from pilkit.processors import ResizeToFill
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    director = models.CharField(max_length=70)
+    actor = models.CharField(max_length=70)
+    comment = models.TextField(blank=True)
+    poster_file = models.ImageField()
+    poster_thumb = ImageSpecField(
+        source="poster_file",
+        processors=[ResizeToFill(400, 800)],
+        format="JPEG",
+        options={"quality": 60},
+    )
+    # TODO : add watched date
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    watched = models.BooleanField()
