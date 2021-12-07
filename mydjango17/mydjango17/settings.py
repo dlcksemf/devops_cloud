90de9672cd4a793d71e76b7bf3045cc84ea4e2ff
Chandeul Lee <22chandl@gmail.com>
2021-12-07 10:40:11 +0900

    diary 앱 생성 및 등록

--- mydjango17/mydjango17/settings.py
+++ mydjango17/mydjango17/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
     # third apps
     # local apps
+    'diary',
 ]
 
 if DEBUG:
