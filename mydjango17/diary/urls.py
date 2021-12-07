15fcc20f26b4c9256e7eb15782bab4a6a2f3b937
Chandeul Lee <22chandl@gmail.com>
2021-12-07 13:34:56 +0900

    post_detail 뷰 구현

--- mydjango17/diary/urls.py
+++ mydjango17/diary/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from diary.views import post_list
+from diary.views import post_list, post_detail
 
 app_name = "diary"
 
 urlpatterns = [
     path('', post_list, name="post_list"),
+    path('<int:pk>/', post_detail, name="post_detail"),
 ]
--- mydjango17/diary/views.py
+++ mydjango17/diary/views.py
@@ -6,5 +6,10 @@ from diary.models import Post, Comment, Tag
 def post_list(request: HttpRequest) -> HttpResponse:
     qs = Post.objects.all()
     context = {"post_list": qs,}
-
     return render(request, "diary/post_list.html", context)
+
+
+def post_detail(request:HttpRequest, pk: int) -> HttpResponse:
+    post = Post.objects.get(pk=pk)
+    context = {"post": post,}
+    return render(request, "diary/post_detail.html", context)
