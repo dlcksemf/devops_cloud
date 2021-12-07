9795bd181236f5ad82bce829e77d70c58c17f56c
Chrissy Lee <22chandl@gmail.com>
2021-12-07 22:01:35 +0900

    music 뷰 구현

--- myhomework17/music/urls.py
+++ myhomework17/music/urls.py
@@ -1,4 +1,12 @@
+from django.urls import path
+
+from music.views import music_index, music_detail, tag_detail
+
 
 app_name = 'music'
 
-urlpatterns = []
+urlpatterns = [
+    path('', music_index, name='music_index'),
+    path('<int:pk>/', music_detail, name='music_detail'),
+    path('tags/<str:tag_name>', tag_detail, name='tag_detail')
+]
