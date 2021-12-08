efd2439fa2adad7e0a21eb6cadbe92551a10af26
Chandeul Lee <22chandl@gmail.com>
2021-12-08 15:16:53 +0900

    music > view 파일 구현

--- mydjango18/music/urls.py
+++ mydjango18/music/urls.py
@@ -1,5 +1,11 @@
+from django.urls import path
+from music.views import music_list, music_detail, tag_detail
 
 app_name = "music"
 
 
-urlpatterns = []
+urlpatterns = [
+    path("", music_list, name="music_list" ),
+    path("/<int:pk>", music_detail, name="music_detail" ),
+    path("tag/<str:tag_name>", tag_detail, name="tag_detail"),
+]
--- mydjango18/music/views.py
+++ mydjango18/music/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from music.models import Music, Comment, Tag
 
-# Create your views here.
+
+def music_list(request:HttpRequest) -> HttpResponse:
+    qs = Music.objects.all()
+    context = {"music_list": qs,}
+    return render(request, "music/music_list.html", context)
+
+
+def music_detail(request:HttpRequest, pk:int) -> HttpResponse:
+    music = Music.objects.get(pk=pk)
+    comment_list = music.comment_set.all()
+    tag_list = music.tag_set.all()
+    context = {
+        "music": music,
+        "comment_list": comment_list,
+        "tag_list": tag_list,
+    }
+    return render(request, "music/music_detail.html", context)
+
+
+def tag_detail(request:HttpRequest, tag_name:str) -> HttpResponse:
+    qs = Music.objects.all()
+    qs = qs.filter(tag_set__name=tag_name)
+    context = {"music_list": qs}
+    return render(request, "music/tag_detail.html", context)
