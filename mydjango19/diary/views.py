ebc682152166cc761595acd9830f2f590e676093
Chandeul Lee <22chandl@gmail.com>
2021-12-09 10:22:37 +0900

    mydjango17 프로젝트를 복제하여 mydjango19 프로젝트 생성

---
+++ mydjango19/diary/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from diary.models import Post, Comment, Tag
+
+
+def post_list(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()
+    query = request.GET.get("query", "")
+
+    if query:
+        qs = qs.filter(title__icontains=query)
+
+    context = {"post_list": qs,}
+    return render(request, "diary/post_list.html", context)
+
+
+def post_detail(request:HttpRequest, pk: int) -> HttpResponse:
+    post = Post.objects.get(pk=pk)
+    comment_list = post.comment_set.all()
+    tag_list = post.tag_set.all()
+    context = {"post": post,
+               "comment_list": comment_list,
+               "tag_list": tag_list,
+               }
+    return render(request, "diary/post_detail.html", context)
+
+
+def tag_detail(request:HttpRequest, tag_name:str ) -> HttpResponse:
+    qs = Post.objects.all()
+    qs = qs.filter(tag_set__name=tag_name)
+    context = {"post_list": qs}
+    return render(request, "diary/tag_detail.html", context)
