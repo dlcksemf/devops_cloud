3e19b323d93c9f10936ebc85ef8baea40ead7525
Chandeul Lee <22chandl@gmail.com>
2021-12-09 13:57:42 +0900

    post_edit 뷰 구현

--- mydjango19/diary/urls.py
+++ mydjango19/diary/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from diary.views import post_list, post_detail, tag_detail, post_new
+from diary.views import post_list, post_detail, tag_detail, post_new, post_edit
 
 app_name = "diary"
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', post_list, name="post_list"),
     path('<int:pk>/', post_detail, name="post_detail"),
     path("new/", post_new, name="post_new"),
+    path("<int:pk>/edit/", post_edit, name="post_edit"),
     path('tags/<str:tag_name>/', tag_detail, name="tag_detail"),
 ]
