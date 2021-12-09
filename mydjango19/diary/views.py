cdbd57a9564088ac39fa9168da393b26107e359c
Chandeul Lee <22chandl@gmail.com>
2021-12-09 15:29:49 +0900

    post_new / post_edit뷰에서 messages를 남기고, 부모템플릿에서 messages를 소비

--- mydjango19/diary/views.py
+++ mydjango19/diary/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 
@@ -41,6 +42,7 @@ def post_new(request: HttpRequest) -> HttpResponse:
             post = form.save(commit=False)
             post.ip = request.META["REMOTE_ADDR"]
             post.save()
+            messages.success(request, "성공적으로 저장 했습니다!")
             return redirect("diary:post_list")
     else:
         form = PostForm()
@@ -58,6 +60,7 @@ def post_edit(request: HttpRequest, pk:int) -> HttpResponse:
         form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             form.save()
+            messages.success(request, "성공적으로 수정 했습니다!")
             return redirect("diary:post_list")
     else:
         form = PostForm(instance=post)
