9326cb4860d11e3869113cf07ef001b4adb60aed
Chandeul Lee <22chandl@gmail.com>
2021-12-10 17:08:15 +0900

    admin 설정

--- mydjango20/shop/admin.py
+++ mydjango20/shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from shop.models import Shop, Review, Tag
 
-# Register your models here.
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["shop"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
