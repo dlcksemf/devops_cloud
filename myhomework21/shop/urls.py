eedb55c0971847a63d65479c862520286982876d
Chrissy Lee <22chandl@gmail.com>
2021-12-13 21:20:40 +0900

    shop_detail, shop_list, shop_form 뷰 구현

--- myhomework21/shop/urls.py
+++ myhomework21/shop/urls.py
@@ -1,6 +1,12 @@
+from django.urls import path
+from shop.views import shop_new, shop_edit, shop_list, shop_detail
+
 
 app_name = "shop"
 
 urlpatterns = [
-
+    path('', shop_list, name="shop_list"),
+    path('<int:pk>/', shop_detail, name="shop_detail"),
+    path('new/', shop_new, name="shop_new"),
+    path('<int:shop_pk>/edit/', shop_edit, name="shop_edit"),
 ]
--- myhomework21/shop/views.py
+++ myhomework21/shop/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from shop.models import Shop, Category, Tag
+from shop.forms import ShopForm
 
-# Create your views here.
+
+def shop_list(request: HttpRequest) -> HttpResponse:
+    qs = Shop.objects.all()
+    category_qs = Category.objects.all()
+
+    query = request.GET.get("query", "")
+    if query:
+        qs = qs.filter(name__icontains=query)
+
+    context = {
+        "shop_list": qs,
+        "category_list": category_qs,
+    }
+    return render(request, "shop/shop_list.html", context)
+
+
+def shop_detail(request: HttpRequest, pk:int) -> HttpResponse:
+    shop = get_object_or_404(Shop, pk=pk)
+    tag_list = shop.tag_set.all()
+    context = {
+        "shop": shop,
+        "tag_list": tag_list,
+    }
+    return render(request, "shop/shop_detail.html", context)
+
+
+def shop_new(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ShopForm(request.POST)
+        if form.is_valid():
+            saved_post = form.save()
+            return redirect("shop:shop_detail", saved_post.pk)
+
+    else:
+        form = ShopForm()
+
+    context = {
+        "form": form,
+    }
+    return render(request, "shop/shop_form.html", context)
+
+
+def shop_edit(request: HttpRequest, shop_pk:int) -> HttpResponse:
+    shop = get_object_or_404(Shop, pk=shop_pk)
+
+    if request.method == "POST":
+        form = ShopForm(request.POST, instance=shop)
+        if form.is_valid():
+            saved_post = form.save()
+            return redirect("shop:shop_detail", saved_post.pk)
+    else:
+        form = ShopForm(instance=shop)
+
+    context = {
+        "form": form,
+    }
+    return render(request, "shop/shop_form.html", context)
