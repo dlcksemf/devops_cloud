a453a22a0657fca86ca663af470a4403d6d8a684
Chandeul Lee <22chandl@gmail.com>
2021-12-13 16:48:31 +0900

    ShopForm의 Tags 필드 초기값을 생성자를 통해서 지정

--- mydjango20/shop/forms.py
+++ mydjango20/shop/forms.py
@@ -5,6 +5,19 @@ from django import forms
 class ShopForm(forms.ModelForm):
     tags = forms.CharField()
 
+    #부모 클래스의 생성자에서 어떤 인자를 지원하는 지는 잘 모르겠지만,
+    # 생성자 호출 시에 받은 이자 그대로
+    # 부모에게 전달하겠다.
+
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # tags는 우리가 Form 클래스에 직접 추가한 필드니까
+        # 초기 값도 우리가 직접 지정 해 주어야 합니다.
+        if self.instance.pk: # 수정 시
+            tag_qs = self.instance.tag_set.all()
+            tags = ', '.join([tag.name for tag in tag_qs])
+            self.fields["tags"].initial = tags
+
     def save(self):
         # 부모의 save를 호출해주어야 합니다.
         saved_post = super().save()
