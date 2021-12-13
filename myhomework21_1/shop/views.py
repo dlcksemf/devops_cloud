10c3785651d68819da2067d0f2d9d00067c70246
Chandeul Lee <22chandl@gmail.com>
2021-12-13 18:56:32 +0900

    views 설정 및 url 연결

--- myhomework21_1/shop/views.py
+++ myhomework21_1/shop/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+from shop.forms import ShopForm
+from shop.models import Shop, Category, Tag
+
+
+def shop_list(request: HttpRequest) -> HttpResponse:
+    qs = Shop.objects.all()  # .order_by("-id")
+    category_qs = Category.objects.all()
+
+    query = request.GET.get("query", "")
+    if query:
+        qs = qs.filter(name__icontains=query)
+
+    category_id: str = request.GET.get("category_id", "")
+    if category_id:
+        qs = qs.filter(category__pk=category_id)
+
+    return render(request, "shop/shop_list.html", {
+        "shop_list": qs,
+        "category_list": category_qs,
+    })
+
+
+# /shop/100/
+def shop_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    shop = get_object_or_404(Shop, pk=pk)
+    return render(request, "shop/shop_detail.html", {
+        "shop": shop,
+    })
+
+
+# /shop/new/
+def shop_new(request: HttpRequest) -> HttpResponse:
+    # raise NotImplementedError("곧 구현 예정")
+
+    if request.method == "POST":
+        form = ShopForm(request.POST, request.FILES)
+        if form.is_valid():
+            saved_post = form.save()
+
+            # shop_detail 뷰를 구현했다면 !!!
+            return redirect("shop:shop_detail", saved_post.pk)
+    else:
+        form = ShopForm()
+
+    return render(request, "shop/shop_form.html", {
+        "form": form,
+    })
+
+
+def shop_edit(request: HttpRequest, pk: int) -> HttpResponse:
+    shop = get_object_or_404(Shop, pk=pk)
+
+    if request.method == "POST":
+        form = ShopForm(request.POST, request.FILES, instance=shop)
+        if form.is_valid():
+            saved_shop = form.save()
+            return redirect("shop:shop_detail", saved_shop.pk)
+    else:
+        form = ShopForm(instance=shop)
+
+    return render(request, "shop/shop_form.html", {
+        "form": form,
+    })
