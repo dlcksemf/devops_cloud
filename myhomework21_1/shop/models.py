468e0555b6ec98d5f52c4985d85e7e1e39ff34b5
Chandeul Lee <22chandl@gmail.com>
2021-12-13 18:45:36 +0900

    Shop / Category / Tag 모델 생성 및 ShopForm 폼 생성

--- myhomework21_1/shop/models.py
+++ myhomework21_1/shop/models.py
@@ -1,3 +1,48 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["-id"]
+
+
+class Shop(TimestampedModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    description = models.TextField(blank=True)
+    telephone = models.CharField(max_length=14,
+                                 validators=[
+                                     RegexValidator(r"^\d{3}-?\d{4}-?\d{4}$", message="전화번호를 입력해주세요."),
+                                 ],
+                                 help_text="입력예) 042-1234-1234")
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["-id"]
+
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
