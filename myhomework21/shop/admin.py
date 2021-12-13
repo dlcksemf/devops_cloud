36000545b9c872e670a44e974dbd850fba9b133d
Chrissy Lee <22chandl@gmail.com>
2021-12-13 20:48:23 +0900

    Shop Category Tag 모델 생성 및 마이그레이트 / 어드민 등록

--- myhomework21/shop/admin.py
+++ myhomework21/shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from shop.models import Shop, Category, Tag
 
-# Register your models here.
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
--- myhomework21/shop/models.py
+++ myhomework21/shop/models.py
@@ -1,3 +1,37 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class TimeStamped(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(TimeStamped):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Shop(TimeStamped):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, db_index=True)
+    telephone = models.CharField(max_length=13,
+                                 validators=[RegexValidator(r"^\d{3}-?\d{4}-?\d{4}$",
+                                                           message="전화번호를 입력해주세요")],
+                                 help_text="예시) 042-1234-1234")
+    description = models.TextField(blank=True)
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Tag(TimeStamped):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
