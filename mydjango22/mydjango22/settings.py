2da3ae4324fc037660102d1ca3b13c493db321e5
Chandeul Lee <22chandl@gmail.com>
2021-12-14 10:32:11 +0900

    music 앱 생성 및 초기 설정

--- mydjango22/mydjango22/settings.py
+++ mydjango22/mydjango22/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third apps
-    'bootstrap5'
+    'bootstrap5',
     # local apps
+    'music',
 ]
 
 if DEBUG:
