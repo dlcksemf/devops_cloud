3a4febe869d6b46051b087ea7405febf6edca6a2
Chandeul Lee <22chandl@gmail.com>
2021-12-14 10:36:59 +0900

    Music, Category, Tag 모델 생성 및 마이그레이트

--- mydjango22/music/models.py
+++ mydjango22/music/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStamped(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Music(TimeStamped):
+    title = models.CharField(max_length=50, db_index=True)
+    artist = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-id"]
+
+
+class Tag(TimeStamped):
+    name = models.CharField(max_length=20, unique=True)
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Category(TimeStamped):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
