750ce0e373a042c6c974e627aab91801aeac3528
Chandeul Lee <22chandl@gmail.com>
2021-12-14 18:42:40 +0900

    view, url 구현

--- myhomework22/shop/views.py
+++ myhomework22/shop/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from shop.models import Shop, Category, Tag
+from shop.forms import ShopForm
 
-# Create your views here.
+
+def shop_list(request: HttpRequest) -> HttpResponse:
+    shop_list = Shop.objects.all()
+    category_list = Category.objects.all()
+
+    context = {
+        "shop_list": shop_list,
+        "category_list": category_list,
+    }
+    return render(request, "shop/shop_list.html", context)
+
+
+def shop_detail(request: HttpRequest, pk:int) -> HttpResponse:
+    shop = get_object_or_404(Shop, pk=pk)
+    tag_list = shop.tag_set.all()
+    context = {
+        "shop": shop,
+        "tag_list": tag_list,
+    }
+    return render(request, "shop/shop_detail.html", context)
+
+
+def shop_new(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ShopForm(request.POST)
+        if form.is_valid():
+            saved_shop = form.save
+            return redirect("shop:shop_detail", saved_shop.pk)
+    else:
+        form = ShopForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "shop/shop_form.html", context)
+
+
+def shop_edit(request: HttpRequest, pk:int) -> HttpResponse:
+    shop = get_object_or_404(Shop, pk=pk)
+    if request.method == "POST":
+        form = ShopForm(request.POST, instance=shop)
+        if form.is_valid():
+            saved_shop = form.save
+            return redirect("shop:shop_detail", saved_shop.pk)
+    else:
+        form = ShopForm(instance=shop)
+    context = {
+        "form": form,
+    }
+    return render(request, "shop/shop_form.html", context)
