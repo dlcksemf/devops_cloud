578856bd1ce1151fed36b38a61bea34cf8ef2ae0
Chandeul Lee <22chandl@gmail.com>
2021-12-15 17:33:44 +0900

    Post, Comment, Category, Tag 모델 생성 및 마이그레이트 // urls > root 설정

--- myhomework23_1/blog/models.py
+++ myhomework23_1/blog/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(TimeStampedModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Post(TimeStampedModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, db_index=True)
+    content = models.TextField()
+    photo = models.ImageField(upload_to="blog/post/%Y/%m/%d")
+    tag_set = models.ManyToManyField("Tag", blank=True)
+    status = models.CharField(
+        max_length=1,
+        choices=[
+            ("D", "Draft"),
+            ("P", "Post")
+        ],
+        db_index=True,
+        default="D",
+    )
+
+    class Meta:
+        ordering = ["-id"]
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(TimeStampedModel):
+    name = models.CharField(max_length=20, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(TimeStampedModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+
+    class Meta:
+        ordering = ["-id"]
+
+    def __str__(self):
+        return f"{self.post} // {self.message}"
