4081cc4977c96153f4daa82d74c9df093a7244e3
Chandeul Lee <22chandl@gmail.com>
2021-12-15 13:55:12 +0900

    post delete 구현

--- mydjango23/blog/views.py
+++ mydjango23/blog/views.py
@@ -59,7 +59,17 @@ def post_edit(request: HttpRequest, pk: int) -> HttpResponse:
 
 
 def post_delete(request: HttpRequest, pk: int) -> HttpResponse:
-    raise NotImplementedError("삭제는 아직 강의에서 다루지 않았습니다.")
+    # GET 요청 : 정말 삭제르 할 것인지, 한 번 더 물어봅니다.
+    # POST 요청 : 삭제를 하고, 다른 주소로 이동을 시킵니다.
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete() # 실제로 DB에 DELETE 쿼리 실행
+        messages.success(request, f"#{pk} 포스팅을 삭제했습니다.")
+        return redirect("blog:post_list")
+
+    return render(request, "blog/post_confirm_delete.html", {
+        "post": post,
+    })
 
 
 def comment_new(request: HttpRequest) -> HttpResponse:
