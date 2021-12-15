928870c5f97c8c54914c4e59ec36f2e9769f5c07
Chandeul Lee <22chandl@gmail.com>
2021-12-15 18:36:43 +0900

    signup 구현

--- myhomework23_1/accounts/views.py
+++ myhomework23_1/accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def login(request):
+    pass
+
+
+def profile(request):
+    pass
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("blog:post_list")
+    else:
+        form = UserCreationForm()
+    return render(request, "accounts/signup_form.html", {
+        "form": form
+    })
+
+
+def logout(request):
+    pass
