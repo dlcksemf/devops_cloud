01ab19f2c3fe3bca14cbdbbff6dc6e24529af01b
Chandeul Lee <22chandl@gmail.com>
2021-12-16 18:45:26 +0900

    tag 문자열로 받을 수 있도록 설정 / 어드민 등록

--- myhomework24/shop/forms.py
+++ myhomework24/shop/forms.py
@@ -1,8 +1,29 @@
 from django import forms
-from shop.models import Shop, Comment
+from shop.models import Shop, Comment, Tag
 
 
 class ShopForm(forms.ModelForm):
+    tags = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            tag_qs = self.instance.tag_set.all()
+            tags = ", ".join([tag.name for tag in tag_qs])
+            self.fields["tags"].initial = tags
+
+    def _save_m2m(self):
+        saved_shop = super()._save_m2m()
+        tag_list = []
+        tags = self.cleaned_data.get("tags", "")
+        for word in tags.split(","):
+            tag_name = word.strip()
+            tag, __ = Tag.objects.get_or_create(name=tag_name)
+            tag_list.append(tag)
+
+        self.instance.tag_set.clear()
+        self.instance.tag_set.add(*tag_list)
+
     class Meta:
         model = Shop
         fields = "__all__"
