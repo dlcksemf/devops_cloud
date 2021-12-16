51749700ee250269708794a1306a1e54029b10fd
Chandeul Lee <22chandl@gmail.com>
2021-12-16 17:35:33 +0900

    view / url 구현

--- myhomework24/shop/urls.py
+++ myhomework24/shop/urls.py
@@ -1,6 +1,15 @@
+from django.urls import path
+from shop import views
 
 app_name = "shop"
 
 urlpatterns = [
-
+    path('', views.shop_list, name="shop_list"),
+    path('<int:pk>/', views.shop_detail, name="shop_detail"),
+    path('new/', views.shop_new, name="shop_new"),
+    path('<int:pk>/edit/', views.shop_edit, name="shop_edit"),
+    path('<int:pk>/delete/', views.shop_delete, name="shop_delete"),
+    path('<int:pk>/comment/new/', views.comment_new, name="comment_new"),
+    path('<int:post_pk>/comment/<int:pk>/edit/', views.comment_edit, name="comment_edit"),
+    path('<int:post_pk>/comment/<int:pk>/delete/', views.comment_delete, name="comment_delete"),
 ]
--- myhomework24/shop/views.py
+++ myhomework24/shop/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from shop.models import Shop, Tag, Comment, Category
+from shop.forms import ShopForm, CommentForm
+
+
+shop_list = ListView.as_view(
+    model = Shop
+)
+
+
+shop_detail = DetailView.as_view(
+    model = Shop
+)
+
+shop_new = CreateView.as_view(
+    model = Shop,
+    form_class = ShopForm,
+)
+
+shop_edit = UpdateView.as_view(
+    model = Shop,
+    form_class = ShopForm,
+)
+
+shop_delete = DeleteView.as_view(
+    model = Shop,
+)
+
+comment_new = CreateView.as_view(
+    model = Comment,
+    form_class= CommentForm,
+)
+
+comment_edit = UpdateView.as_view(
+    model = Comment,
+    form_class= CommentForm,
+)
+
+
+comment_delete = DeleteView.as_view(
+    model = Comment,
+)
