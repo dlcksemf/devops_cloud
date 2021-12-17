bd894eea98b98d50633fa6db116d42976533a803
Chandeul Lee <22chandl@gmail.com>
2021-12-17 16:46:12 +0900

    pagination 구현

--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -11,6 +11,7 @@ from shop.forms import ShopForm, ReviewForm
 
 class ShopListView(ListView):
     model=Shop
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
