555a04e61b937c3487d640ca348d43c53a46cb31
Chandeul Lee <22chandl@gmail.com>
2021-12-17 10:49:28 +0900

    accounts 앱 생성 및 초기 설정

--- mydjango25/mydjango25/urls.py
+++ mydjango25/mydjango25/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="root.html"), name="root"),
     path('shop/', include('shop.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
