9ae5f7eadd68b7e3815f94fac7e5324b74a0242d
Chandeul Lee <22chandl@gmail.com>
2021-12-17 13:20:44 +0900

    프로필, 로그아웃 구현

--- mydjango25/accounts/views.py
+++ mydjango25/accounts/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 
 signup = CreateView.as_view(
     form_class=UserCreationForm,
@@ -9,8 +10,17 @@ signup = CreateView.as_view(
     template_name="accounts/signup_form.html",
 )
 
-
 login = LoginView.as_view(
     template_name='accounts/login.html',
+)
+
+# TODO : 커스텀 CBV를 만든다면, LoginRequiredMixin을 상속 받도록 할 수 있습니다.
+profile = login_required(TemplateView.as_view(
+    template_name="accounts/profile.html"
+)
+)
+
+logout = LogoutView.as_view(
+    next_page = "root",
 )
 
