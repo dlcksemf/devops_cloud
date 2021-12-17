51d60db80821cbe748da210e00868fff2bb2e4dd
Chandeul Lee <22chandl@gmail.com>
2021-12-17 18:11:17 +0900

    login 구현

--- myhomework25/accounts/views.py
+++ myhomework25/accounts/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
 
-# Create your views here.
+signup = CreateView.as_view(
+    form_class=UserCreationForm,
+    template_name="accounts/signup.html",
+)
+
+login = LoginView.as_view()
+
+profile = TemplateView.as_view()
+
+logout = LogoutView.as_view()
