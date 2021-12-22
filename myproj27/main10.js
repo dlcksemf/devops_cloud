b0aa8bbefa66b6a0020321f19822400b3d1b9c53
Chandeul Lee <22chandl@gmail.com>
2021-12-22 13:47:39 +0900

    sum()함수 구현 및 적용

--- myproj27/main10.js
+++ myproj27/main10.js
@@ -1,15 +1,27 @@
 const { melon_data: song_array } = require("./melon_data");
 
+Array.prototype.sum = function () {
+    return this.reduce((acc, element) => acc + element, 0);
+};
+
 // TODO: #10 방탄소년단의 좋아요의 총 합은?
 // Array의 filter와 reduce를 활용해주세요.
 // ref: https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
 
 console.log("--- TODO # 10 ---");
 
-const BTS_likes = song_array.filter(
+const result = song_array.filter(
     (song) => (song.artist === "방탄소년단")
-).reduce(
-    (prev, curr) => (prev + curr.like), 0
-);
+).map(({like}) => like)
+.sum();
 
-console.log(BTS_likes);
+console.log(result)
+
+
+// const BTS_likes = song_array.filter(
+//     (song) => (song.artist === "방탄소년단")
+// ).reduce(
+//     (prev, curr) => (prev + curr.like), 0
+// );
+
+// console.log(BTS_likes);
