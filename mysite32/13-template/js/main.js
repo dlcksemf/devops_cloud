138b7f6ebbb83cbbf1cf97253fe5964868ca38b9
Chandeul Lee <22chandl@gmail.com>
2021-12-29 12:43:00 +0900

    책 > pinterest 실습

---
+++ mysite32/13-template/js/main.js
@@ -0,0 +1,39 @@
+const frame = "selection";
+const box = "article";
+const speed = "0.5s";
+const activeClass = "on";
+const btn = document.querySelectorAll("main ul li");
+
+let grid;
+
+window.addEventListener("load", () => {
+  init();
+  filter(btn);
+});
+
+function int() {
+  grid = new Istope(frame, {
+    itemSelector: box,
+    columnWidth: box,
+    transitionDuration: speed,
+  });
+}
+
+function filter(arr) {
+  for (let el of arr) {
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+      const sort = e.currentTarget.querySelector("a").getAttribute("href");
+
+      grid.arrange({
+        filter: sort,
+      });
+
+      for (let el of arr) {
+        el.classList.remove(activeClass);
+      }
+
+      e.currentTarget.classList.add(activeClass);
+    });
+  }
+}
