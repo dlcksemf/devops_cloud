ba8c22f67daa55cd4309cbf4b75eebb0cde41046
Chandeul Lee <22chandl@gmail.com>
2021-12-29 13:34:39 +0900

    데이터 이름 바꿔주기

--- myreact33/src/pages/PageProfile.jsx
+++ myreact33/src/pages/PageProfile.jsx
@@ -4,36 +4,42 @@ import Axios from 'axios';
 function PageProfile() {
   const [profileList, setProfileList] = useState([]);
 
-  useEffect(
-    () =>
-      Axios.get(
-        'https://classdevopscloud.blob.core.windows.net/data/profile-list.json',
-      )
-        .then((response) => {
-          setProfileList(response.data);
-        })
-        .catch((error) => console.error(error)),
-    [],
-  );
+  useEffect(() => {
+    Axios.get(
+      'https://classdevopscloud.blob.core.windows.net/data/profile-list.json',
+    )
+      .then((response) => {
+        const profileList = response.data.map((profile) => ({
+          ...profile,
+          profileImageUrl: profile.profile_image_url,
+          instagramUrl: profile.instagram_url,
+        }));
+        setProfileList(profileList);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <div>
       <h2>Page Profile</h2>
+
       {profileList.length === 0 && <h3>등록된 프로필이 없습니다.</h3>}
 
       {profileList.map((member) => {
         return (
-          <>
+          <div key={member.unique_id}>
             <h3>{member.name}</h3>
 
-            <img src={member.profile_image_url} alt="" />
+            <img src={member.profileImageUrl} alt="" />
 
             <ul>
               <li>{member.role}</li>
               <li>{member.mbti}</li>
-              <li>{member.instagram_url}</li>
+              <li>{member.instagramUrl}</li>
             </ul>
-          </>
+          </div>
         );
       })}
     </div>
