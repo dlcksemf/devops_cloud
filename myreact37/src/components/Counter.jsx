31a7899823dd0487511d4b0f6a2d9956acd04f97
Chandeul Lee <22chandl@gmail.com>
2022-01-05 10:50:22 +0900

    클릭시 value 값 증가 / 오른쪽 클릭시 value 값 감소 -- useReducer 사용하여 구현

--- myreact37/src/components/Counter.jsx
+++ myreact37/src/components/Counter.jsx
@@ -1,12 +1,40 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import './Counter.css';
 
+const ACTION_TYPE = {
+  CLICKED: 'CLICKED',
+  RIGHT_CLICKED: 'RIGHT_CLICKED',
+};
+
+function reducer(prevValue, action) {
+  const { type } = action;
+  switch (type) {
+    case ACTION_TYPE.CLICKED:
+      return prevValue + 1;
+    case ACTION_TYPE.RIGHT_CLICKED:
+      return prevValue - 1;
+    default:
+      return prevValue;
+  }
+}
+
 function Counter() {
-  const [value, setValue] = useState(0);
+  const [value, dispatch] = useReducer(reducer, 0);
+
+  const handleMinus = () => dispatch({ type: ACTION_TYPE.RIGHT_CLICKED });
 
   return (
     <>
-      <div className="Counter">{value}</div>
+      <div
+        className="Counter"
+        onClick={() => dispatch({ type: ACTION_TYPE.CLICKED })}
+        onContextMenu={(e) => {
+          e.preventDefault();
+          handleMinus();
+        }}
+      >
+        {value}
+      </div>
     </>
   );
 }
